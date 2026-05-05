10ee563c902d6b1b1c17934ef4d60da29a21500d
DanielRhee <danielrheewa@gmail.com>
2026-05-05 09:28:57 -0700

    better cuda management

--- nautilus/start.sh
+++ nautilus/start.sh
@@ -18,14 +18,13 @@ source "$HOME/miniconda3/etc/profile.d/conda.sh"
 BASHRC
 
 CC=$(nvidia-smi --query-gpu=compute_cap --format=csv,noheader | head -1 | tr -d '.')
-if   [ "$CC" -ge 90 ]; then TORCH_IDX="cu124"; SPCONV="spconv-cu124"
-elif [ "$CC" -ge 80 ]; then TORCH_IDX="cu121"; SPCONV="spconv-cu120"
-else                        TORCH_IDX="cu118"; SPCONV="spconv-cu118"
+if   [ "$CC" -ge 80 ]; then TORCH_IDX="cu124"; SPCONV="spconv-cu124==2.3.8"
+else                        TORCH_IDX="cu118"; SPCONV="spconv-cu118==2.3.6"
 fi
 
 conda run -n lidarflow python -m pip install torch --index-url https://download.pytorch.org/whl/${TORCH_IDX}
 conda run -n lidarflow python -m pip install av2
-conda run -n lidarflow python -m pip install ${SPCONV}
+conda run -n lidarflow python -m pip install "${SPCONV}"
 
 conda activate lidarflow
 
